33a590b37368d2373783a4563f49fadf770b5dd0
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-04-22 14:34:55 +0300

    Profiling fragment created with own viewModel

---
+++ app/src/main/java/com/example/kiraaz/ui/profiling/ProfilingFragment.kt
@@ -0,0 +1,142 @@
+package com.example.kiraaz.ui.profiling
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.kiraaz.R
+import com.example.kiraaz.databinding.FragmentProfilingBinding
+import com.example.kiraaz.utils.Constants
+import java.util.*
+
+class ProfilingFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfilingBinding
+    private lateinit var viewModel: ProfilingViewModel
+
+    private var selectedImage: Uri? = null
+    private var isImageChanged = false
+
+    //private lateinit var args : ProfileInfoFragmentArgs
+    private var isNewAccount = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[ProfilingViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfilingBinding.inflate(inflater, container, false)
+
+        //pick image from gallery
+        binding.profileIv.setOnClickListener {
+            Toast.makeText(requireContext(), "Image clicked", Toast.LENGTH_SHORT).show()
+            pickPhotoFromGallery()
+            isImageChanged = true
+        }
+
+        //city dropdown
+        val cityItems = Constants.cities
+        val cityAutoComplete: AutoCompleteTextView = binding.cityDropdown
+        val cityAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_items, cityItems)
+        cityAutoComplete.setAdapter(cityAdapter)
+
+        //date picker
+        binding.birthdateTv.setOnClickListener {
+            val getDate = Calendar.getInstance()
+            val datePicker = DatePickerDialog(
+                requireContext(),
+                { _, year, month, dayOfMonth ->
+                    val date = "$dayOfMonth/${month + 1}/$year"
+                    binding.birthdateTv.text = date
+                },
+                getDate.get(Calendar.YEAR),
+                getDate.get(Calendar.MONTH),
+                getDate.get(Calendar.DAY_OF_MONTH)
+            )
+
+            datePicker.datePicker.maxDate = System.currentTimeMillis()
+
+            if (binding.birthdateTv.text != "Birthdate") {
+                val date = binding.birthdateTv.text.toString().split("/")
+                datePicker.updateDate(date[2].toInt(), date[1].toInt() - 1, date[0].toInt())
+            }
+
+            datePicker.show()
+            binding.birthdateEt.visibility = View.VISIBLE
+        }
+
+
+        return binding.root
+    }
+
+    @SuppressLint("IntentReset")
+    private fun pickPhotoFromGallery() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            startActivityForResult(intent, 2)
+
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            pickPhotoFromGallery()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            selectedImage = data.data
+            if (selectedImage != null && Build.VERSION.SDK_INT >= 28) {
+                val source =
+                    ImageDecoder.createSource(requireActivity().contentResolver, selectedImage!!)
+                val bitmap = ImageDecoder.decodeBitmap(source)
+                binding.profileIv.setImageBitmap(bitmap)
+            } else {
+                val bitmap = MediaStore.Images.Media.getBitmap(
+                    requireActivity().contentResolver,
+                    selectedImage
+                )
+                binding.profileIv.setImageBitmap(bitmap)
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
---
+++ app/src/main/java/com/example/kiraaz/ui/profiling/ProfilingViewModel.kt
@@ -0,0 +1,127 @@
+package com.example.kiraaz.ui.profiling
+
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kiraaz.model.Profile
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+
+class ProfilingViewModel : ViewModel() {
+
+    private val _mAuth = FirebaseAuth.getInstance()
+    val mAuth: FirebaseAuth
+        get() = _mAuth
+
+    private val _uid = _mAuth.currentUser?.uid
+    val uid: String
+        get() = _uid!!
+
+    private val database = FirebaseFirestore.getInstance()
+
+    private val storage = FirebaseStorage.getInstance()
+
+    //Upload Profile
+    private val _isUploaded = MutableLiveData<Boolean>()
+    val isUploaded: MutableLiveData<Boolean>
+        get() = _isUploaded
+
+    private val _profile = MutableLiveData<Profile>()
+    val profile: MutableLiveData<Profile>
+        get() = _profile
+
+    fun upload(name : String, gender : String, birthdate : String, city: String, problems: ArrayList<String>){
+
+        _mAuth.currentUser?.updateProfile(
+            UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .build()
+        )
+
+        _profile.value = Profile(name,gender,birthdate,city,
+            _mAuth.currentUser?.email,_mAuth.currentUser?.photoUrl.toString(),problems)
+
+        database.collection("Profiles").document(_uid!!).set(_profile.value!!).addOnSuccessListener {
+            _isUploaded.value = true
+        }.addOnFailureListener {
+            _isUploaded.value = false
+        }
+
+    }
+
+    //Upload Profile Image
+    private val _isImageUploaded = MutableLiveData<Boolean>()
+    val isImageUploaded: MutableLiveData<Boolean>
+        get() = _isImageUploaded
+
+    fun uploadImage(selectedImage : Uri){
+        val imageName = _uid
+        val imageRef = storage.reference.child("images/${imageName}.jpg")
+
+        imageRef.putFile(selectedImage).addOnSuccessListener {
+
+            //set firebase user display photo
+            _mAuth.currentUser?.updateProfile(
+                UserProfileChangeRequest.Builder()
+                    .setPhotoUri(selectedImage)
+                    .build()
+            )
+
+            _profile.value?.image = selectedImage.toString()
+
+            imageRef.downloadUrl.addOnSuccessListener {
+                database.collection("Profiles").document(_uid!!).update("image", it.toString())
+            }
+
+            _isImageUploaded.value = true
+        }.addOnFailureListener {
+            _isImageUploaded.value = false
+        }
+    }
+
+    //New User
+    private val _isNewUser = MutableLiveData<Boolean>()
+    val isNewUser: MutableLiveData<Boolean>
+        get() = _isNewUser
+
+    fun checkNewUser(){
+        database.collection("Profiles").document(_uid!!).get().addOnSuccessListener {
+            _isNewUser.value = !it.exists()
+        }
+    }
+
+    //Get Profile
+    private val _isDownloaded = MutableLiveData<Boolean>()
+    val isDownloaded: MutableLiveData<Boolean>
+        get() = _isDownloaded
+
+    val errorDownload = MutableLiveData<String>()
+
+    fun download(){
+        database.collection("Profiles").document(_uid!!).addSnapshotListener { data, error ->
+
+            if(error != null){
+                _isDownloaded.value = false
+                errorDownload.value = error.message
+            }else{
+
+                if(data != null){
+                    val name = _mAuth.currentUser?.displayName
+                    val email = _mAuth.currentUser?.email
+                    val image = _mAuth.currentUser?.photoUrl.toString()
+                    val gender = data["gender"].toString()
+                    val birthDate = data["birthDate"].toString()
+                    val city = data["city"].toString()
+                    val problems = data["problems"] as ArrayList<String>?
+
+                    _profile.value = Profile(name,gender, birthDate, city, email, image, problems)
+
+                    _isDownloaded.value = true
+                }
+            }
+
+        }
+    }
+}
