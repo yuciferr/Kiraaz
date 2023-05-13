24441b4f044ad6e2d919ab85af99178ea4b5e8a1
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-13 22:07:54 +0300

    Create: ProfileFragment.kt and ProfileViewModel.kt

---
+++ app/src/main/java/com/example/kiraaz/ui/profile/ProfileFragment.kt
@@ -0,0 +1,100 @@
+package com.example.kiraaz.ui.profile
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.net.toUri
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.kiraaz.R
+import com.example.kiraaz.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
+    }
+
+    //Show bottom navigation bar
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val bottomNavigationBar = activity?.findViewById<View>(R.id.bottomNavigationView)
+        bottomNavigationBar?.visibility = View.VISIBLE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK){
+            Configuration.UI_MODE_NIGHT_NO -> {
+                // Light theme is active, switch to dark
+                binding.themeBtn.text = "Dark Mode"
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                // Dark theme is active, switch to light
+                binding.themeBtn.text = "Light Mode"
+            }
+            else -> {
+                // Follow system theme
+                binding.themeBtn.text = "System Mode"
+            }
+        }
+
+
+        //database get
+        viewModel.download()
+        viewModel.isDownloaded.observe(viewLifecycleOwner){
+            if(it){
+                binding.nameTv.text = viewModel.profile.value?.name
+                binding.profileIv2.setImageURI(viewModel.profile.value?.image?.toUri())
+                binding.genderTv.text = viewModel.profile.value?.gender
+            }else{
+                Toast.makeText(context, viewModel.errorDownload.value, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        binding.profileInfoBtn.setOnClickListener {
+            val action = ProfileFragmentDirections.actionProfileFragmentToProfilingFragment(isNewAccount = false)
+            findNavController().navigate(action)
+        }
+
+        binding.themeBtn.setOnClickListener{
+            //change theme
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    // Light theme is active, switch to dark
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    // Dark theme is active, switch to light
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+                }
+                else -> {
+                    // Follow system theme
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+            }
+        }
+
+        binding.signoutBtn.setOnClickListener{
+            viewModel.mAuth.signOut()
+            findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+        }
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/kiraaz/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.kiraaz.ui.profile
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kiraaz.model.Profile
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ProfileViewModel : ViewModel() {
+
+    private val _mAuth = FirebaseAuth.getInstance()
+    val mAuth: FirebaseAuth
+        get() = _mAuth
+
+    private val uid = _mAuth.currentUser?.uid
+
+    private val database = FirebaseFirestore.getInstance()
+
+    //Get Profile
+    private val _profile = MutableLiveData<Profile>()
+    val profile: MutableLiveData<Profile>
+        get() = _profile
+
+
+    private val _isDownloaded = MutableLiveData<Boolean>()
+    val isDownloaded: MutableLiveData<Boolean>
+        get() = _isDownloaded
+
+    val errorDownload = MutableLiveData<String>()
+
+    fun download() {
+        database.collection("Profiles").document(uid!!).addSnapshotListener { data, error ->
+
+            if (error != null) {
+                _isDownloaded.value = false
+                errorDownload.value = error.message
+            } else {
+
+                if (data != null) {
+                    val name = _mAuth.currentUser?.displayName
+                    val image = _mAuth.currentUser?.photoUrl.toString()
+                    val gender = data["gender"].toString()
+
+                    _profile.value = Profile(name, gender, null,null,null, image, null)
+
+                    _isDownloaded.value = true
+                }
+            }
+
+        }
+    }
+
+}
