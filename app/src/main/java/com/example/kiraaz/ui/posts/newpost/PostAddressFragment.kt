8e45b2f0078d54e2391d8ef03d7c88c7a3d70e86
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-19 15:22:12 +0300

    Update: mapView zoom in current location

--- app/src/main/java/com/example/kiraaz/ui/posts/newpost/PostAddressFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/posts/newpost/PostAddressFragment.kt
@@ -1,21 +1,33 @@
 package com.example.kiraaz.ui.posts.newpost
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentPostAddressBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
+import java.util.*
 
+@Suppress("DEPRECATION")
 class PostAddressFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentPostAddressBinding
 
     private lateinit var mapView: MapView
+    private lateinit var map: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     //Hide bottom navigation bar
     @Deprecated("Deprecated in Java")
@@ -34,6 +46,7 @@ class PostAddressFragment : Fragment(), OnMapReadyCallback {
         mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         binding.nextBtn.setOnClickListener {
             findNavController().navigate(R.id.action_postAddressFragment_to_postDetailFragment)
@@ -42,9 +55,63 @@ class PostAddressFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val location = LatLng(41.0082, 28.9784)
-        googleMap.addMarker(MarkerOptions().position(location).title("İstanbul"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 13f))
+        map = googleMap
+        setUpMap()
+        map.setOnMapLongClickListener(listener)
+
+    }
+
+    private fun setUpMap(){
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+            return
+        }
+        map.isMyLocationEnabled = false
+        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                addMarker(currentLatLng)
+            }
+        }
+    }
+
+    private fun addMarker(currentLatLng: LatLng){
+        map.clear()
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+        var address = ""
+        try {
+            val addressList = geocoder.getFromLocation(currentLatLng.latitude, currentLatLng.longitude, 1)
+            if (addressList?.size!! > 0) {
+
+                if (addressList[0].thoroughfare != null) {
+                    address += addressList[0].thoroughfare
+                    if (addressList[0].subThoroughfare != null) {
+                        address += addressList[0].subThoroughfare
+
+                    }
+                }
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        map.addMarker(MarkerOptions().position(currentLatLng).title(address))
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+    }
+
+    private val listener = GoogleMap.OnMapLongClickListener { p0 ->
+        addMarker(p0)
     }
 
     // MapView lifecycle methods
@@ -52,29 +119,37 @@ class PostAddressFragment : Fragment(), OnMapReadyCallback {
         super.onResume()
         mapView.onResume()
     }
+
     override fun onStart() {
         super.onStart()
         mapView.onStart()
     }
+
     override fun onStop() {
         super.onStop()
         mapView.onStop()
     }
+
     override fun onPause() {
         super.onPause()
         mapView.onPause()
     }
+
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
     }
+
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
     }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
     }
-
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
 }
