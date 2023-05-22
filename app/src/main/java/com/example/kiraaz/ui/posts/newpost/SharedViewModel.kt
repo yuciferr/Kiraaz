dabd14881b86daa4b45fbeb8b0e051bd303b78ce
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-22 20:17:35 +0300

    Add: HomePost uploading firebase database and unload SharedViewModel.kt

--- app/src/main/java/com/example/kiraaz/ui/posts/newpost/SharedViewModel.kt
+++ app/src/main/java/com/example/kiraaz/ui/posts/newpost/SharedViewModel.kt
@@ -11,6 +11,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SharedViewModel : ViewModel() {
 
@@ -51,24 +53,97 @@ class SharedViewModel : ViewModel() {
         address.value = Address(latLng, street, city, district)
     }
 
-    private fun setHome(floor: Int, rooms: Int, isAmericanKitchen: Boolean, isFurnished: Boolean) {
+    private fun setHome(
+        floor: Int,
+        rooms: String,
+        isAmericanKitchen: Boolean,
+        isFurnished: Boolean
+    ) {
         _home.value =
             Home(imageLinks.value!!, address.value!!, floor, rooms, isAmericanKitchen, isFurnished)
     }
 
     fun uploadImages() {
+        val imageList = ArrayList<String>()
         for (i in 0 until _images.value!!.size) {
             if (_images.value!![i] != null) {
-                val imageName = _uid + "home" + (i+1)
+                val imageName = "home" + UUID.randomUUID().toString() + (i + 1)
                 val imageRef = storage.reference.child("images/${imageName}.jpg")
 
                 imageRef.putFile(_images.value!![i]!!).addOnSuccessListener {
                     imageRef.downloadUrl.addOnSuccessListener {
-                        imageLinks.value?.add(it.toString())
+                        imageList.add(it.toString())
                     }
                 }
             }
         }
+        imageLinks.value = imageList
+    }
+
+    fun uploadHomePost(
+        title: String,
+        description: String?,
+        price: Int,
+        deposit: Int?,
+        roommate: Int,
+        floor: Int,
+        rooms: String,
+        isAmericanKitchen: Boolean,
+        isFurnished: Boolean
+    ) {
+        setHome(floor, rooms, isAmericanKitchen, isFurnished)
+
+        val calendar = Calendar.getInstance()
+        val date = "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH)}/${
+            calendar.get(
+                Calendar.YEAR
+            )
+        }"
+
+        val id = UUID.randomUUID().toString()
+
+        _homePost.value = HomePost(
+            _uid!!,
+            _home.value!!,
+            title,
+            description,
+            price,
+            deposit,
+            0,
+            roommate,
+            date,
+            id,
+            true
+        )
+
+        database.collection("HomePosts").document(id).set(_homePost.value!!).addOnSuccessListener {
+            Log.d("HomePost", "HomePost added")
+        }.addOnFailureListener {
+            Log.d("HomePost", "HomePost not added")
+        }
+    }
+
+    fun unLoad() {
+        _images.value = arrayListOf()
+        _isAddImage.value =
+            arrayListOf(false, false, false, false, false, false, false, false, false)
+        imageLinks.value = arrayListOf()
+        address.value = Address(LatLng(0.0, 0.0), "", "", "")
+        _home.value =
+            Home(arrayListOf(), Address(LatLng(0.0, 0.0), "", "", ""), 0, "", false, false)
+        _homePost.value = HomePost(
+            "",
+            Home(arrayListOf(), Address(LatLng(0.0, 0.0), "", "", ""), 0, "", false, false),
+            "",
+            "",
+            0,
+            0,
+            0,
+            0,
+            "",
+            "",
+            true
+        )
     }
 
 }
