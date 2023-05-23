2df6bb707123164d55cd816a1f4f282933adea39
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-23 22:29:56 +0300

    Create: DetailFragment.kt

---
+++ app/src/main/java/com/example/kiraaz/ui/detail/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.kiraaz.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kiraaz.R
+import com.example.kiraaz.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private lateinit var binding : FragmentDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
