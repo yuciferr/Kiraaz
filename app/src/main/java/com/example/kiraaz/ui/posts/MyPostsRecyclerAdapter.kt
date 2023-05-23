e63c4a033e2124e5176a7904e6f7c59fa8925cf6
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-23 18:16:05 +0300

    Create: MyPostsRecyclerAdapter.kt and bind home_post_item.xml

---
+++ app/src/main/java/com/example/kiraaz/ui/posts/MyPostsRecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.kiraaz.ui.posts
+
+import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.kiraaz.R
+import com.example.kiraaz.databinding.HomePostItemBinding
+import com.example.kiraaz.model.HomePost
+
+class MyPostsRecyclerAdapter(private val items : List<HomePost?>) : RecyclerView.Adapter<MyPostsRecyclerAdapter.MyPostsViewHolder>(){
+
+    class MyPostsViewHolder(private val binding: HomePostItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(homePost: HomePost?) {
+            binding.apply {
+                titleTv.text = homePost?.title
+                priceTv.text = homePost?.price.toString()
+                val location = homePost?.home?.address?.district + ", " + homePost?.home?.address?.city
+                locationTv.text = location
+                profileIv.setImageURI(homePost?.ownerPicture?.toUri())
+                Glide.with(root.context).load(homePost?.home?.images?.get(0)?.toUri()).into(postIv)
+                favoriteBtn.setImageResource(R.drawable.round_create_16)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPostsViewHolder {
+        val binding = HomePostItemBinding.inflate(
+            parent.context.getSystemService(android.view.LayoutInflater::class.java),
+            parent,
+            false
+        )
+        return MyPostsViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: MyPostsViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+}
