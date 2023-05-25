a7218d3ee26b6c0fcc636ba50389474403b0c343
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-25 16:07:16 +0300

    Add: ViewModel an RecyclerView set

--- app/src/main/java/com/example/kiraaz/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/search/SearchFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentSearchBinding
+import com.example.kiraaz.model.HomePost
 
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var viewModel: SearchViewModel
+
+    private var homePosts: List<HomePost?> = listOf()
 
     //Show bottom navigation bar
     @Deprecated("Deprecated in Java")
@@ -20,11 +26,29 @@ class SearchFragment : Fragment() {
         bottomNavigationBar?.visibility = View.VISIBLE
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[SearchViewModel::class.java]
+
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        homePosts = viewModel.homePosts.value ?: listOf()
+
+        viewModel.getPosts()
+        viewModel.homePosts.observe(viewLifecycleOwner) { posts ->
+            homePosts = posts
+            binding.searchRv.adapter = SearchRecyclerAdapter(homePosts)
+            binding.searchRv.layoutManager = LinearLayoutManager(requireContext())
+        }
+
 
         return binding.root
     }
