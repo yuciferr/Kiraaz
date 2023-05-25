e870693ee28946735b369c136f56f18300ae65f5
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-25 14:05:44 +0300

    Add: Bottom Navigation setup

--- app/src/main/java/com/example/kiraaz/ui/MainActivity.kt
+++ app/src/main/java/com/example/kiraaz/ui/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.kiraaz.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.kiraaz.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.findNavController()!!
+        bottomNavigationBar.setupWithNavController(navController)
     }
 }
