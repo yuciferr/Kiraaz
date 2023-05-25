0e930941d002ea6285cd64a47f0f418631edd7c4
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-25 11:29:53 +0300

    Create: Favorites Fragment

---
+++ app/src/main/java/com/example/kiraaz/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,34 @@
+package com.example.kiraaz.ui.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kiraaz.R
+import com.example.kiraaz.databinding.FragmentFavoritesBinding
+
+
+class FavoritesFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoritesBinding
+
+    //Show bottom navigation bar
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val bottomNavigationBar = activity?.findViewById<View>(R.id.bottomNavigationView)
+        bottomNavigationBar?.visibility = View.VISIBLE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+}
