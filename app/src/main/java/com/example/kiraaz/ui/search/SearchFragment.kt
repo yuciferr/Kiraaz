ac1ab302e817080a609e93a2118da393a8e70572
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-26 21:51:11 +0300

    Add: SearchView implementation

--- app/src/main/java/com/example/kiraaz/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,12 +12,15 @@ import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentSearchBinding
 import com.example.kiraaz.model.HomePost
 
+
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
     private lateinit var viewModel: SearchViewModel
 
     private var homePosts: List<HomePost?> = listOf()
+    private var isEmpty : Boolean = true
+
 
     //Show bottom navigation bar
     @Deprecated("Deprecated in Java")
@@ -41,17 +45,36 @@ class SearchFragment : Fragment() {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
 
         homePosts = viewModel.homePosts.value ?: listOf()
+        isEmpty = viewModel.isEmpty.value ?: true
 
-        viewModel.getPosts()
+        if (isEmpty) {
+            viewModel.getPosts()
+        }
         viewModel.homePosts.observe(viewLifecycleOwner) { posts ->
             homePosts = posts
+            binding.progressBar2.visibility = View.GONE
+            binding.searchRv.visibility = View.VISIBLE
             binding.searchRv.adapter = SearchRecyclerAdapter(homePosts)
-            binding.searchRv.layoutManager = LinearLayoutManager(requireContext())
+            binding.searchRv.layoutManager = LinearLayoutManager(context)
         }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.progressBar2.visibility = View.VISIBLE
+                binding.searchRv.visibility = View.GONE
+                viewModel.getPosts(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+
 
 
         return binding.root
     }
-
-
 }
