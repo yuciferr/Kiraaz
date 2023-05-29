524ad99482d665305416d3cbc2f75ab8ed1ad177
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-05-29 23:58:11 +0300

    Fix: READ_EXTERNAL_STORAGE permission bug

--- app/src/main/java/com/example/kiraaz/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/profile/ProfileFragment.kt
@@ -1,14 +1,17 @@
 package com.example.kiraaz.ui.profile
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.kiraaz.R
@@ -38,66 +41,77 @@ class ProfileFragment : Fragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK){
-            Configuration.UI_MODE_NIGHT_NO -> {
-                // Light theme is active, switch to dark
-                binding.themeBtn.text = "Dark Mode"
-            }
-            Configuration.UI_MODE_NIGHT_YES -> {
-                // Dark theme is active, switch to light
-                binding.themeBtn.text = "Light Mode"
-            }
-            else -> {
-                // Follow system theme
-                binding.themeBtn.text = "System Mode"
-            }
-        }
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
 
-
-        //database get
-        viewModel.download()
-        viewModel.isDownloaded.observe(viewLifecycleOwner){
-            if(it){
-                binding.nameTv.text = viewModel.profile.value?.name
-                binding.profileIv2.setImageURI(viewModel.profile.value?.image?.toUri())
-                binding.genderTv.text = viewModel.profile.value?.gender
-            }else{
-                Toast.makeText(context, viewModel.errorDownload.value, Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-        binding.profileInfoBtn.setOnClickListener {
-            val action = ProfileFragmentDirections.actionProfileFragmentToProfilingFragment(isNewAccount = false)
-            findNavController().navigate(action)
-        }
-
-        binding.themeBtn.setOnClickListener{
-            //change theme
-            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK){
                 Configuration.UI_MODE_NIGHT_NO -> {
                     // Light theme is active, switch to dark
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    binding.themeBtn.text = "Dark Mode"
                 }
                 Configuration.UI_MODE_NIGHT_YES -> {
                     // Dark theme is active, switch to light
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
+                    binding.themeBtn.text = "Light Mode"
                 }
                 else -> {
                     // Follow system theme
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    binding.themeBtn.text = "System Mode"
                 }
             }
-        }
 
-        binding.postBtn.setOnClickListener{
-            findNavController().navigate(R.id.action_profileFragment_to_myPostsFragment)
-        }
 
-        binding.signoutBtn.setOnClickListener{
-            viewModel.mAuth.signOut()
-            findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+            //database get
+            viewModel.download()
+            viewModel.isDownloaded.observe(viewLifecycleOwner){
+                if(it){
+                    binding.nameTv.text = viewModel.profile.value?.name
+                    binding.profileIv2.setImageURI(viewModel.profile.value?.image?.toUri())
+                    binding.genderTv.text = viewModel.profile.value?.gender
+                }else{
+                    Toast.makeText(context, viewModel.errorDownload.value, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            binding.profileInfoBtn.setOnClickListener {
+                val action = ProfileFragmentDirections.actionProfileFragmentToProfilingFragment(isNewAccount = false)
+                findNavController().navigate(action)
+            }
+
+            binding.themeBtn.setOnClickListener{
+                //change theme
+                when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        // Light theme is active, switch to dark
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        // Dark theme is active, switch to light
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+                    }
+                    else -> {
+                        // Follow system theme
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    }
+                }
+            }
+
+            binding.postBtn.setOnClickListener{
+                findNavController().navigate(R.id.action_profileFragment_to_myPostsFragment)
+            }
+
+            binding.signoutBtn.setOnClickListener{
+                viewModel.mAuth.signOut()
+                findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+            }
+
         }
         return binding.root
     }
