a1580c54d1c31540ca51e50c49d3863f93645b57
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-06-04 13:24:20 +0300

    Add: FavoritesBtn click listener

--- app/src/main/java/com/example/kiraaz/ui/favorites/FavoritesRecyclerAdapter.kt
+++ app/src/main/java/com/example/kiraaz/ui/favorites/FavoritesRecyclerAdapter.kt
@@ -8,10 +8,17 @@ import com.bumptech.glide.Glide
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.ItemHomePostBinding
 import com.example.kiraaz.model.HomePost
+import com.example.kiraaz.ui.search.SearchFragmentDirections
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FavoritesRecyclerAdapter(private val items : List<HomePost?>) : RecyclerView.Adapter<FavoritesRecyclerAdapter.FavoritesViewHolder>(){
 
     class FavoritesViewHolder(private val binding: ItemHomePostBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        private val mAuth = FirebaseAuth.getInstance()
+        private val uid = mAuth.currentUser?.uid.toString()
+        private val database = FirebaseFirestore.getInstance()
 
         fun bind(homePost: HomePost?) {
             binding.apply {
@@ -23,6 +30,46 @@ class FavoritesRecyclerAdapter(private val items : List<HomePost?>) : RecyclerVi
                 Glide.with(root.context).load(homePost?.home?.images?.get(0)?.toUri()).into(postIv)
                 favoriteBtn.setImageResource(R.drawable.round_favorite_24)
             }
+        }
+
+        fun navigateToDetailFragment(item : HomePost?) {
+            binding.postIv.setOnClickListener {
+                val action = SearchFragmentDirections.actionSearchFragmentToDetailFragment(item!!)
+                binding.root.findNavController().navigate(action)
+            }
+        }
+
+        fun favoritePost(item : HomePost?) {
+            binding.favoriteBtn.setOnClickListener {
+                database.collection("Profiles")
+                    .document(uid)
+                    .collection("Favorites")
+                    .whereEqualTo("id", item?.id)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        if (documents.isEmpty) {
+                            database.collection("Profiles")
+                                .document(uid)
+                                .collection("Favorites")
+                                .document(item?.id.toString())
+                                .set(item!!)
+                                .addOnSuccessListener {
+                                    binding.favoriteBtn.setImageResource(R.drawable.round_favorite_24)
+                                }
+                        } else {
+                            database.collection("Profiles")
+                                .document(uid)
+                                .collection("Favorites")
+                                .document(item?.id.toString())
+                                .delete()
+                                .addOnSuccessListener {
+                                    binding.favoriteBtn.setImageResource(R.drawable.round_favorite_border_24)
+                                }
+                        }
+                    }
+            }
+
+
         }
 
     }
@@ -43,9 +90,7 @@ class FavoritesRecyclerAdapter(private val items : List<HomePost?>) : RecyclerVi
     override fun onBindViewHolder(holder: FavoritesViewHolder, position: Int) {
         holder.bind(items[position])
 
-        holder.itemView.setOnClickListener {
-            val action = FavoritesFragmentDirections.actionFavoritesFragmentToDetailFragment(items[position]!!)
-            holder.itemView.findNavController().navigate(action)
-        }
+        holder.favoritePost(items[position])
+        holder.navigateToDetailFragment(items[position])
     }
 }
