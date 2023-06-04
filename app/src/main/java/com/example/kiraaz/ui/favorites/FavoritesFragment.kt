94f3e4a3780fd9ad6d8d0cea0c8f6d74041d0ff0
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-06-04 13:20:50 +0300

    Add: FavoritesViewModel and RecyclerView

--- app/src/main/java/com/example/kiraaz/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/favorites/FavoritesFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentFavoritesBinding
+import com.example.kiraaz.model.HomePost
 
 
 class FavoritesFragment : Fragment() {
 
     private lateinit var binding: FragmentFavoritesBinding
+    private lateinit var viewModel: FavoritesViewModel
+
+    private var homePosts: List<HomePost?> = listOf()
+    private var isEmpty : Boolean = true
 
     //Show bottom navigation bar
     @Deprecated("Deprecated in Java")
@@ -21,11 +28,34 @@ class FavoritesFragment : Fragment() {
         bottomNavigationBar?.visibility = View.VISIBLE
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[FavoritesViewModel::class.java]
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        homePosts = viewModel.homePosts.value ?: listOf()
+        isEmpty = viewModel.isEmpty.value ?: true
+
+        if (isEmpty) {
+            viewModel.getFavorites()
+            binding.recyclerview.visibility = View.GONE
+            binding.group.visibility = View.VISIBLE
+        }
+        viewModel.homePosts.observe(viewLifecycleOwner) { posts ->
+            homePosts = posts
+            binding.group.visibility = View.GONE
+            binding.recyclerview.visibility = View.VISIBLE
+            binding.recyclerview.adapter = FavoritesRecyclerAdapter(homePosts)
+            binding.recyclerview.layoutManager = LinearLayoutManager(context)
+        }
+
 
         return binding.root
     }
