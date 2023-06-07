229993e6a9fb420179dc0030fe13a5682cf50995
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-06-07 16:57:53 +0300

    Add: Args and viewmodel

--- app/src/main/java/com/example/kiraaz/ui/messages/chat/ChatFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/messages/chat/ChatFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
+    private lateinit var viewModel: ChatViewModel
+    private val navArgs by navArgs<ChatFragmentArgs>()
 
     //Hide bottom navigation bar
     @Deprecated("Deprecated in Java")
@@ -20,11 +25,31 @@ class ChatFragment : Fragment() {
         bottomNavigationBar?.visibility = View.GONE
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[ChatViewModel::class.java]
+        //viewModel.getMessages(navArgs.ownerID)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentChatBinding.inflate(inflater, container, false)
+
+
+        binding.apply {
+
+            profileIv3.setImageURI(navArgs.ownerPhoto.toUri())
+            nameTv.text = navArgs.ownerName
+
+
+            sendBtn.setOnClickListener {
+                val message = messageTv.text.toString()
+                viewModel.sendMessage(navArgs.ownerID, message)
+                messageTv.text?.clear()
+            }
+        }
 
         return binding.root
     }
