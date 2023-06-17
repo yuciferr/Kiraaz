ffeecee08eb019669fd2b387e0a9aed534e75f88
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-06-17 17:05:25 +0300

    Add: chat recycler view and back actions

--- app/src/main/java/com/example/kiraaz/ui/messages/chat/ChatFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/messages/chat/ChatFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentChatBinding
 
@@ -28,30 +30,44 @@ class ChatFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[ChatViewModel::class.java]
-        //viewModel.getMessages(navArgs.ownerID)
+        viewModel.getMessages(navArgs.ownerID)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentChatBinding.inflate(inflater, container, false)
-
+        setupRecyclerView()
 
         binding.apply {
 
             profileIv3.setImageURI(navArgs.ownerPhoto.toUri())
             nameTv.text = navArgs.ownerName
 
-
             sendBtn.setOnClickListener {
                 val message = messageTv.text.toString()
                 viewModel.sendMessage(navArgs.ownerID, message)
                 messageTv.text?.clear()
+                viewModel.getMessages(navArgs.ownerID)
             }
-        }
 
-        return binding.root
+            backBtn2.setOnClickListener {
+                findNavController().backQueue.removeLast()
+                findNavController().navigateUp()
+            }
+
+            return binding.root
+        }
     }
 
+    private fun setupRecyclerView() {
+        viewModel.getMessages(navArgs.ownerID)
+        viewModel.messages.observe(viewLifecycleOwner) {
+            binding.messagesRv.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = ChatRecyclerAdapter(it)
+                scrollToPosition(it.size - 1)
+            }
+        }
+    }
 }
