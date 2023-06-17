84c5f296bee73663da6b039ef28b9fea1561808d
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-06-17 18:53:18 +0300

    Create: binding recyclerview adapter and viewmodel

--- app/src/main/java/com/example/kiraaz/ui/messages/MessagesFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/messages/MessagesFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentMessagesBinding
+import com.example.kiraaz.model.Chat
 
 
 class MessagesFragment : Fragment() {
 
     private lateinit var binding: FragmentMessagesBinding
+    private lateinit var viewModel: MessagesViewModel
+    private lateinit var chats : List<Chat>
 
 
     //Show bottom navigation bar
@@ -27,6 +32,15 @@ class MessagesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMessagesBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[MessagesViewModel::class.java]
+        viewModel.getChats()
+
+        viewModel.chats.observe(viewLifecycleOwner) {
+            chats = it
+            binding.messagesRv.adapter = MessagesRecyclerAdapter(chats)
+            binding.messagesRv.layoutManager = LinearLayoutManager(requireContext())
+        }
+
 
         return binding.root
     }
