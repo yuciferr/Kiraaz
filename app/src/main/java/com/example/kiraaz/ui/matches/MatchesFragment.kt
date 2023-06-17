aaa80b9c4469f2dc11a105bea9fea75d801aa26c
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-06-17 23:22:40 +0300

    Add: MatchesViewModel.kt and MatchesRecyclerAdapter

--- app/src/main/java/com/example/kiraaz/ui/matches/MatchesFragment.kt
+++ app/src/main/java/com/example/kiraaz/ui/matches/MatchesFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kiraaz.R
 import com.example.kiraaz.databinding.FragmentMatchesBinding
+import com.example.kiraaz.model.Profile
 
 
 class MatchesFragment : Fragment() {
 
     private lateinit var binding: FragmentMatchesBinding
+    private lateinit var viewModel: MatchesViewModel
+    private lateinit var matches: List<Profile>
+    private lateinit var percents: List<Int>
 
     //Show bottom navigation bar
     @Deprecated("Deprecated in Java")
@@ -26,6 +32,22 @@ class MatchesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMatchesBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[MatchesViewModel::class.java]
+        viewModel.updateMatches()
+        percents = listOf(100, 90, 80, 70, 60, 50, 40, 30, 20, 10)
+
+        viewModel.matches.observe(viewLifecycleOwner) {
+            matches = it
+            binding.matchesRv.adapter = MatchesRecyclerAdapter(matches, percents)
+            binding.matchesRv.layoutManager = LinearLayoutManager(
+                context,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            binding.matchesRv.visibility = View.VISIBLE
+            binding.imageView4.visibility = View.GONE
+        }
+
 
         return binding.root
     }
